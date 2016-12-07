57374a2f56da71f9bcc8b465a79c8abc9661803b
a.kompaniets <alexkomp.ua@gmail.com>
2016-12-07 14:08:20 +0200

    add reset

--- redux.js
+++ redux.js
@@ -1,3 +1,4 @@
+// @flow
 import { NavigationExperimental } from 'react-native';
 
 import shallowCompare from './utils/shallowCompare';
@@ -7,8 +8,33 @@ const { StateUtils } = NavigationExperimental;
 const KEY = 'router';
 const ROUTER_PUSH = `${KEY}/push`;
 const ROUTER_POP = `${KEY}/pop`;
+const ROUTER_RESET = `${KEY}/reset`;
 
-function push(payload) {
+type Route = {
+  key: string,
+  params?: Object,
+};
+
+type State = {
+  index: ?number,
+  routes: Route[],
+};
+
+type Handler<T> = {
+  [key: string]: (state: T, action: Action<*>) => T,
+};
+
+type Action<T> = {
+  type: string,
+  payload: T,
+};
+
+type ResetPayload = {
+  routes: Route | Route[],
+  index?: number,
+};
+
+function push(payload: Route) {
   return {
     type: ROUTER_PUSH,
     payload,
@@ -21,46 +47,57 @@ function pop() {
   };
 }
 
-const initialState = {
+function reset(payload: ResetPayload) {
+  return {
+    type: ROUTER_RESET,
+    payload,
+  };
+}
+
+const initialState: State = {
   index: null,
-  params: {},
   routes: [],
 };
 
-const actionHandlers = {
-  [ROUTER_PUSH]: (state, action) => {
+const actionHandlers: Handler<State> = {
+  [ROUTER_PUSH]: (state, action: Action<Route>): State => {
+    const newRoute = action.payload;
     if (state.routes.length === 0 ) {
-      const newState = StateUtils.push(state, action.payload);
-      newState.params = action.payload.params ? action.payload.params : {};
-      return newState;
+      return StateUtils.push(state, newRoute);
     }
 
-    const sameRoute = state.routes[state.index].key === (action.payload && action.payload.key);
-    const payloadDiffers = action.payload.params && !shallowCompare(state.params, action.payload.params);
+    const currentRoute = state.routes[state.index];
+    const sameRoute = currentRoute.key === (newRoute && newRoute.key);
+    const payloadDiffers = currentRoute.params
+      && newRoute.params && !shallowCompare(currentRoute.params, newRoute.params);
 
     if (sameRoute && !payloadDiffers) {
       return state;
     }
 
-    const newState = sameRoute && payloadDiffers
-      ? StateUtils.replaceAt(state, action.payload.key, action.payload)
-      : StateUtils.push(state, action.payload);
-
-    newState.params = action.payload.params ? action.payload.params : {};
-    return newState
+    return sameRoute && payloadDiffers
+      ? StateUtils.replaceAt(state, newRoute.key, newRoute)
+      : StateUtils.push(state, newRoute);
   },
 
-  [ROUTER_POP]: (state, action) => state.index > 0 ? StateUtils.pop(state) : state,
+  [ROUTER_POP]: (state, action: Action<void>): State => state.index > 0 ? StateUtils.pop(state) : state,
+  [ROUTER_RESET]: (state, action: Action<ResetPayload>): State => {
+    const { routes, index } = action.payload;
+    const newRoutes = Array.isArray(routes) ? routes : [routes];
+    return StateUtils.reset(state, newRoutes, index);
+  },
 };
 
 const actionTypes = {
   ROUTER_PUSH,
   ROUTER_POP,
+  ROUTER_RESET,
 };
 
 const actionCreators = {
   push,
   pop,
+  reset,
 };
 
 export {
@@ -69,7 +106,7 @@ export {
   actionCreators,
 };
 
-export default (state = initialState, action) => {
+export default (state: State = initialState, action: Action<*>): State => {
   const handler = actionHandlers[action.type];
 
   return handler ? handler(state, action) : state;
