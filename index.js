83b8b5eaf8700ad206345831eae19025a72f68c3
Oxyaction <alexkomp.ua@gmail.com>
2017-03-16 20:34:45 +0200

    fix import

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import Router from './src/RouterComponent';
-import routerReducer, { actionCreators, actionTypes, initialState } from './src/redux';
+import routerReducer, { actionCreators, actionTypes, initialState, selectors } from './src/redux';
 
 export {
   routerReducer,
   actionCreators,
   actionTypes,
   initialState,
+  selectors,
 };
 
 export default Router;
